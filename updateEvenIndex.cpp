7380a22bd6fdf4b16f23ef6104be53a537ed8491
Sanidhya Pant <152873379+sanidhyapant98@users.noreply.github.com>
2025-02-06 17:02:04 +0530

    Add files via upload
    updating even indices to 'a'

---
+++ updateEvenIndex.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	string str;
+	getline(cin,str);
+	int i=0;
+	while(str[i]!='\0'){
+		if(i%2==0){
+			str[i]='a';
+		}
+		i++;
+	}
+	cout<<str;
+
+	return 0;
+}
